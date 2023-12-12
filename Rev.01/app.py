ca2ca8267e1a47bdf64e9e59a515c32e54d193ef
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2023-12-12 15:31:45 +0900

    Update app.py

--- Rev.01/app.py
+++ Rev.01/app.py
@@ -5,6 +5,36 @@ import os
 from PIL import Image
 import io
 
+def find_similar_circles(image, threshold_value=1):
+   # Convert the image to grayscale
+   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+   # Apply Gaussian blur to reduce noise
+   blurred = cv2.medianBlur(gray, 13)
+
+   _, dst = cv2.threshold(blurred, threshold_value, 255, cv2.THRESH_TOZERO)
+
+   circles = cv2.HoughCircles(dst, cv2.HOUGH_GRADIENT, dp=1, minDist=400,
+                              param1=40, param2=10, minRadius=150, maxRadius=180)
+​
+   valid_circles = []
+   if circles is not None:
+       circles = np.round(circles[0, :]).astype(int)
+       for (x, y, r) in circles:
+           circle_mask = np.zeros_like(image)
+           cv2.circle(circle_mask, (x, y), r, (255, 255, 255), -1)
+           circle_roi = cv2.bitwise_and(image, circle_mask)
+          
+           circle_gray = cv2.cvtColor(circle_roi, cv2.COLOR_BGR2GRAY)
+           _, circle_binary = cv2.threshold(circle_gray, 170, 200, cv2.THRESH_TOZERO)
+
+           white_pixels = cv2.countNonZero(circle_binary)
+
+           if white_pixels < 100:
+               valid_circles.append((x, y, r))
+
+return np.array(valid_circles, dtype=np.uint16)
+
 # Streamlit 페이지 설정
 st.title('Image Processing App')
 st.write('이 애플리케이션은 보라색 원을 찾아서 표기합니다.')
